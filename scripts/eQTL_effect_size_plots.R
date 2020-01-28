640f1d0650e46a20f89dd75a73c2b4eaa399211c
Roshni Patel <rpatel7@stanford.edu>
2020-01-27 16:34:03 -0800

    update Rscripts to deal with new file structure

--- scripts/eQTL_effect_size_plots.R
+++ scripts/eQTL_effect_size_plots.R
@@ -3,52 +3,52 @@
 
 library(tidyverse)
 
-anchet = read_tsv("data/hits/hits_estimation_anc_het.txt")
-afr = read_tsv("data/hits/hits_estimation_Afr.txt")
-eur = read_tsv("data/hits/hits_estimation_Eur.txt")
+het = read_tsv("results/v2/hits/hits_estimation_het.txt")
+afr = read_tsv("results/v2/hits/hits_estimation_Afr.txt")
+eur = read_tsv("results/v2/hits/hits_estimation_Eur.txt")
 
-merged = anchet %>% inner_join(afr, by=c("gene", "variant"), suffix=c("_anc_het", "_Afr")) %>% inner_join(eur, by=c("gene", "variant"), suffix=c("", "_Eur")) # Note that the last suffix command doesn't work properly so Eur columns have no suffix appended
+merged = het %>% inner_join(afr, by=c("gene", "variant"), suffix=c("_het", "_Afr")) %>% inner_join(eur, by=c("gene", "variant"), suffix=c("", "_Eur")) # Note that the last suffix command doesn't work properly so Eur columns have no suffix appended
 merged = merged %>% mutate(AfrEur_avg = (slope_Afr + slope) / 2)
 merged = merged %>% mutate(AfrEur_wtavg = (maf_Afr / (maf_Afr + maf)) * slope_Afr + (maf / (maf_Afr + maf)) * slope)
-merged = merged %>% mutate(obs_dif = slope_anc_het - slope, exp_dif_avg = AfrEur_avg - slope, exp_dif_wtavg = AfrEur_wtavg - slope)
+merged = merged %>% mutate(obs_dif = slope_het - slope, exp_dif_avg = AfrEur_avg - slope, exp_dif_wtavg = AfrEur_wtavg - slope)
 
 # Plot Afr effect sizes vs Eur effect sizes without confidence intervals
 tib = merged %>% filter(maf_Afr > .05, maf > .05) # Filter for eQTLs with MAF of at least .05 in both estimation datasets
 weights = 1/sqrt((tib %>% pull(slope_se_Afr))^2 + (tib %>% pull(slope_se))^2) # Define weights for inverse variance weighting
-pdf("plots/Afr_vs_Eur_effect_sizes.pdf")
+pdf("results/v2/plots/Afr_vs_Eur_effect_sizes.pdf")
 ggplot(tib, aes(slope, slope_Afr)) + geom_point() + geom_smooth(method=lm, color='blue', aes(weight=weights)) + geom_abline(aes(intercept=0, slope=1)) + labs(x="European effect size", y="African effect size")
 dev.off()
 
 # Plot Afr effect sizes vs Eur effect sizes with confidence intervals
 tib = merged %>% filter(maf_Afr > .05, maf > .05) # Filter for eQTLs with MAF of at least .05 in both estimation datasets
 weights = 1/sqrt((tib %>% pull(slope_se_Afr))^2 + (tib %>% pull(slope_se))^2) # Define weights for inverse variance weighting
-pdf("plots/Afr_vs_Eur_effect_sizes_CI.pdf")
+pdf("results/v2/plots/Afr_vs_Eur_effect_sizes_CI.pdf")
 ggplot(tib, aes(slope, slope_Afr)) + geom_segment(tib, mapping = aes(x=slope -1.96*slope_se, xend=slope + 1.96 * slope_se, y=slope_Afr, yend = slope_Afr), color='gray', alpha=.9, size=.8) + geom_segment(tib, mapping = aes(x=slope, xend=slope, y=slope_Afr - 1.96 * slope_se_Afr, yend = slope_Afr + 1.96 * slope_se_Afr), color='gray', alpha=.9, size=.8) + geom_point() + geom_smooth(method=lm, color='blue', aes(weight=weights)) + geom_abline(aes(intercept=0, slope=1)) + labs(x="European effect size", y="African effect size")
 dev.off()
 
 # Plot observed vs expected deviation from European effect sizes in ancestry-heterozygous individuals
 
 # Use average of Afr and Eur effect sizes to represent expected effect size in ancestry-heterozygous
-weights = merged %>% transmute(varX = slope_se_anc_het^2 + slope_se^2, varY = .25 * (slope_se_Afr ^2 + slope_se ^2)) %>% mutate(weight = 1/sqrt(varX + varY))
-pdf("plots/slope_dif_avg.pdf")
+weights = merged %>% transmute(varX = slope_se_het^2 + slope_se^2, varY = .25 * (slope_se_Afr ^2 + slope_se ^2)) %>% mutate(weight = 1/sqrt(varX + varY))
+pdf("results/v2/plots/slope_dif_avg.pdf")
 ggplot(merged, aes(exp_dif_avg, obs_dif)) + geom_point() + geom_abline(aes(slope=1, intercept=0)) + labs(x="Exp. dif. from Eur. effect size", y="Obs. dif. from Eur. effect size") + geom_smooth(method=lm, color='blue', aes(weight = weights$weight))
 dev.off()
 
 # Use allele frequency-weighted average of Afr and Eur effect sizes to represent expected effect size in ancestry-heterozygous
-weights = merged %>% transmute(varX = slope_se_anc_het^2 + slope_se^2, varY = (maf_Afr / (maf_Afr + maf))^2 * (slope_se_Afr ^2) + (maf / (maf_Afr + maf))^2 * (slope_se ^2)) %>% mutate(weight = 1/sqrt(varX + varY))
-pdf("plots/slope_dif_wtavg.pdf")
+weights = merged %>% transmute(varX = slope_se_het^2 + slope_se^2, varY = (maf_Afr / (maf_Afr + maf))^2 * (slope_se_Afr ^2) + (maf / (maf_Afr + maf))^2 * (slope_se ^2)) %>% mutate(weight = 1/sqrt(varX + varY))
+pdf("results/v2/plots/slope_dif_wtavg.pdf")
 ggplot(merged, aes(exp_dif_wtavg, obs_dif)) + geom_point() + geom_abline(aes(slope=1, intercept=0)) + labs(x="Exp. dif. from Eur. effect size", y="Obs. dif. from Eur. effect size") + geom_smooth(method=lm, color='blue', aes(weight=weights$weight))
 dev.off()
 
 # Plot absolute value of observed vs expected deviation from European effect sizes in ancestry-heterozygous individuals
-tib = merged %>% mutate(obs_dif = abs(slope_anc_het - slope), exp_dif_avg = abs(AfrEur_avg - slope), exp_dif_wtavg = abs(AfrEur_wtavg - slope))
+tib = merged %>% mutate(obs_dif = abs(slope_het - slope), exp_dif_avg = abs(AfrEur_avg - slope), exp_dif_wtavg = abs(AfrEur_wtavg - slope))
 
 # Use average of Afr and Eur effect sizes to represent expected effect size in ancestry-heterozygous
-pdf("plots/slope_abs_dif_avg.pdf")
+pdf("results/v2/plots/slope_abs_dif_avg.pdf")
 ggplot(merged, aes(exp_dif_avg, obs_dif)) + geom_point() + geom_abline(aes(slope=1, intercept=0)) + labs(x="Exp. dif. from Eur. effect size", y="Obs. dif. from Eur. effect size") 
 dev.off()
 
 # Use allele frequency-weighted average of Afr and Eur effect sizes to represent expected effect size in ancestry-heterozygous
-pdf("plots/slope_abs_dif_wtavg.pdf")
+pdf("results/v2/plots/slope_abs_dif_wtavg.pdf")
 ggplot(merged, aes(exp_dif_wtavg, obs_dif)) + geom_point() + geom_abline(aes(slope=1, intercept=0)) + labs(x="Exp. dif. from Eur. effect size", y="Obs. dif. from Eur. effect size")
 dev.off()
--- scripts/identify_hits.R
+++ scripts/identify_hits.R
@@ -1,20 +1,26 @@
-# Run with at least 30G of memory in home directory
+# Run from home directory with at least 2G of memory
 
 library(tidyverse)
+library(qvalue)
 
 dir.create("data/hits")
 asc = read_tsv("data/fastqtl_output/merged_ascertainment_Eur.txt", col_names = c("gene", "num_var", "beta_shape1", "beta_shape2", "true_df", "pval_true_df", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "ref_factor", "pval_nominal", "slope", "slope_se", "pval_perm", "pval_beta")) # Heading pulled from Francois Aguet's FastQTL repo
-eur = read_tsv("data/fastqtl_output/merged_estimation_Eur.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
-afr = read_tsv("data/fastqtl_output/merged_estimation_Afr.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
 
-# Apply Bonferroni correction and MAF filter to ascertainment dataset to identify significant hits
-total_tests = asc %>% pull("num_var") %>% sum
-bonf_threshold = .05 / total_tests
-hits = asc %>% filter(pval_beta < bonf_threshold, maf > .05)
+# Apply Storey-Tibshirani FDR correction and MAF filter to ascertainment dataset to identify significant hits
+asc = asc %>% mutate(fdr = qvalue(pval_beta)$qvalues)
+hits = asc %>% filter(maf >=.05, fdr <= .10)
 hits %>% write.table("data/hits/hits_ascertainment_Eur.txt", sep='\t', row.names=FALSE)
 
 # Extract effect sizes (+ other info) for significant hits from estimation datasets
+# Note that estimation datasets must be read and filtered sequentially otherwise you'll run out of memory
+eur = read_tsv("data/fastqtl_output/merged_estimation_Eur.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
 eur = eur %>% inner_join(hits %>% select(gene, variant))
-eur %>% write.table("data/hits/hits_estimation_Eur.txt", sep='\t', row.names=FALSE)
+eur %>% write.table("results/v2/hits/hits_estimation_Eur.txt", sep='\t', row.names=FALSE)
+
+afr = read_tsv("data/fastqtl_output/merged_estimation_Afr.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
 afr = afr %>% inner_join(hits %>% select(gene, variant))
-afr %>% write.table("data/hits/hits_estimation_Afr.txt", sep='\t', row.names=FALSE)
+afr %>% write.table("results/v2/hits/hits_estimation_Afr.txt", sep='\t', row.names=FALSE)
+
+het = read_tsv("data/fastqtl_output/merged_estimation_het.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
+het = het %>% inner_join(hits %>% select(gene, variant))
+het %>% write.table("results/v2/hits/hits_estimation_het.txt", sep='\t', row.names=FALSE)
