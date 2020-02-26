71d1506f18d037cfdc55cadf155a75278341dec0
Roshni Patel <rpatel7@stanford.edu>
2020-02-25 16:19:42 -0800

    made hit identification script compatible with Snakefile

--- scripts/identify_hits.R
+++ scripts/identify_hits.R
@@ -1,26 +1,38 @@
-# Run from home directory with at least 2G of memory
-
 library(tidyverse)
+library(optparse)
 library(qvalue)
 
-dir.create("data/hits")
-asc = read_tsv("data/fastqtl_output/merged_ascertainment_Eur.txt", col_names = c("gene", "num_var", "beta_shape1", "beta_shape2", "true_df", "pval_true_df", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "ref_factor", "pval_nominal", "slope", "slope_se", "pval_perm", "pval_beta")) # Heading pulled from Francois Aguet's FastQTL repo
+# Parse arguments
+option_list = list(make_option("--asc", type="character", default=NULL,
+                        help="merged ascertainment file name", metavar="character"),
+                   make_option("--afr", type="character", default=NULL,
+                        help="merged afr estimation file name", metavar="character"),
+                   make_option("--eur", type="character", default=NULL,
+                        help="merged eur estimation file name", metavar="character"),
+                   make_option("--het", type="double", default=NULL,
+                        help="merged het estimation file name", metavar="character"),
+                   make_option("--out", type="character", default=NULL,
+                        help="output file name prefix", metavar="character"))
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
 
-# Apply Storey-Tibshirani FDR correction and MAF filter to ascertainment dataset to identify significant hits
-asc = asc %>% mutate(fdr = qvalue(pval_beta)$qvalues)
-hits = asc %>% filter(maf >=.05, fdr <= .10)
-hits %>% write.table("data/hits/hits_ascertainment_Eur.txt", sep='\t', row.names=FALSE)
+asc = read_tsv(opt$asc)
+
+# Apply Storey-Tibshirani FDR correction  to ascertainment dataset to identify significant hits
+asc = asc %>% mutate(fdr = qvalue(perm_pval)$qvalues)
+hits = asc %>% filter(fdr <= .10)
+hits %>% write.table(paste0(opt$out, "hits_ascertainment_Eur.txt"), sep='\t', row.names=FALSE)
 
 # Extract effect sizes (+ other info) for significant hits from estimation datasets
 # Note that estimation datasets must be read and filtered sequentially otherwise you'll run out of memory
-eur = read_tsv("data/fastqtl_output/merged_estimation_Eur.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
+eur = read_tsv(opt$eur)
 eur = eur %>% inner_join(hits %>% select(gene, variant))
-eur %>% write.table("results/v2/hits/hits_estimation_Eur.txt", sep='\t', row.names=FALSE)
+eur %>% write.table(paste0(opt$out, "hits_estimation_Eur.txt", sep='\t', row.names=FALSE)
 
-afr = read_tsv("data/fastqtl_output/merged_estimation_Afr.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
+afr = read_tsv(opt$afr)
 afr = afr %>% inner_join(hits %>% select(gene, variant))
-afr %>% write.table("results/v2/hits/hits_estimation_Afr.txt", sep='\t', row.names=FALSE)
+afr %>% write.table(paste0(opt$out, "hits_estimation_Afr.txt", sep='\t', row.names=FALSE)
 
-het = read_tsv("data/fastqtl_output/merged_estimation_het.txt", col_names = c("gene", "variant", "tss_distance", "ma_samples", "ma_count", "maf", "pval_nominal", "slope", "slope_se"))
+het = read_tsv(opt$het)
 het = het %>% inner_join(hits %>% select(gene, variant))
-het %>% write.table("results/v2/hits/hits_estimation_het.txt", sep='\t', row.names=FALSE)
+het %>% write.table(paste0(opt$out, "hits_estimation_het.txt", sep='\t', row.names=FALSE)
