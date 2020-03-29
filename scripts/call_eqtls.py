d3bbaac13d54f9b6780cde619e0912a77f742efd
Roshni Patel <rpatel7@stanford.edu>
2020-03-29 08:56:42 -0700

    fixed minor bugs in eQTL calling when covariates included

--- scripts/call_eqtls.py
+++ scripts/call_eqtls.py
@@ -1,14 +1,13 @@
 import sys
+import os
 import pandas as pd
 import argparse
+import gzip
 import numpy as np
 import statsmodels.api as sm
 
-# TODO add MAF threshold; include MAF in final output
-# TODO handle covariates
-
 regression_stat_cols = ["effect", "se", "r2", "pval"]
-geno_info_cols = ["ID", "ma_samp", "maf"]
+geno_info_cols = ["ma_samp", "maf"]
 
 def get_ind(sample_filepath):
     # Read sample IDs from file into list
@@ -17,93 +16,106 @@ def get_ind(sample_filepath):
     sample_file.close()
     return(ind)
 
-def parse_expression(ind):
+def parse_expression(exp_path, ind):
     # Parse expression data input and filter for desired sample IDs
-    for line in sys.stdin:
-        if line[0] == '#':
-            header = line.strip().split()
-        else:
-            exp = line.strip().split()
-    exp = pd.DataFrame(exp, columns=header)
+    exp = pd.read_csv(exp_path, sep='\t')
     exp = exp[ind]
+    exp = exp - exp.mean(axis=1).iloc[0] # Mean-center expression data
     return(exp)
 
 def parse_covariates(cov_file_path, ind):
-    cov = pd.read_csv(cov_file_path, delimiter='\t')
+    cov = pd.read_csv(cov_file_path, delimiter='\t', index_col="NWDID").T
     cov = cov[ind]
     return(cov)
 
-def parse_genotypes(vcf_path, ind):
+def parse_genotypes(vcf_path, ind=None):
     # Parse genotype data input and filter for desired sample IDs
     geno_list = []
-    with open(vcf_path, 'r') as geno_file:
+    with gzip.open(vcf_path, 'rt') as geno_file:
         for line in geno_file:
             if line[:2] == "##":
                 continue
             if line[0] == "#":
-                header = line.split()
+                line = line.split()
+                header = [line[2]] + line[9:]
             else:
-                vcf_geno = line.split()
+                line = line.split()
+                ID = line[2]
+                vcf_geno = line[9:]
                 int_geno = [int(gt[0]) + int(gt[2]) for gt in vcf_geno] # Convert genotype strings in VCF file into integer allele counts
-                geno_list.append(int_geno)
-    if not geno_list: # Checks for the case in which there are no SNPs within 100 kb of the given gene's TSS
-        return(None)
-    else:
-        geno = pd.DataFrame(geno_list, columns=header)["ID" + ind] # Drop all columns except for SNP ID and individuals in sample file
-        return(geno)
+                geno_list.append([ID] + int_geno)
+    geno = pd.DataFrame(geno_list, columns=header).set_index(header[0])
+    if ind is not None:
+        geno = geno[ind]
+    return(geno)
 
 def maf_filter(geno, ind, maf_thresh, ma_samp_thresh):
     alleles = geno[ind] # Create separate DataFrame to store allele counts
     
-    # Record number of individuals with minor allele for each SNP
-    geno["ma_samp"] = alleles[alleles != 0].count(axis=1)
+    # Record number of individuals with alternate allele for each SNP
+    geno["alt_samp"] = alleles[alleles != 0].count(axis=1)
     
-    # Record minor allele frequency for each SNP
+    # Record alternate allele frequency for each SNP
     n_ind = len(ind)
-    geno["maf"] = alleles.sum(axis=1).divide(n_ind)
+    geno["alt_af"] = alleles.sum(axis=1).divide(2 * n_ind)
+
+    # Record minor allele frequency and count
+    geno["ma_samp"] = pd.DataFrame([geno["alt_samp"], n_ind - geno["alt_samp"]]).T.min(axis=1)
+    geno["maf"] = pd.DataFrame([geno["alt_af"], 1 - geno["alt_af"]]).T.min(axis=1)
 
     # Remove rows that do not meet both minor allele thresholds
-    geno = geno[(geno.ma_samp >= ma_samp_thresh) & (geno.maf >= maf_thresh)]    
+    geno = geno[(geno.ma_samp >= ma_samp_thresh) & (geno.maf >= maf_thresh)][list(ind) + geno_info_cols]
     return(geno)
 
 def perform_regression(geno, exp, ind):
     # Regress genotypes onto expression data
     def marginal_test(row):
-        model = sm.OLS(exp.iloc[0], row[ind])
+        model = sm.OLS(exp.iloc[0].astype(float), row[ind].astype(float))
         results = model.fit()
         effect = results.params[0]
         se = results.bse[0]
         r2 = results.rsquared
         pval = results.pvalues[0]
-        return(effect, se, pval, r2)
-
-    res = pd.DataFrame()
+        return(effect, se, r2, pval)
+    res = pd.DataFrame(columns=regression_stat_cols, index=geno.index)
     res[regression_stat_cols] = geno.apply(marginal_test, axis=1, result_type='expand')
-    res[geno_info_cols] = geno[geno_info_cols]
     return(res)
 
 def perm_adjust_pval(n, min_pval, geno, exp, ind):
     def permutation(geno, exp, ind):
         randomized_ind = np.random.permutation(ind)
+        n_ind = len(ind)
+        dummy_ind = list(range(n_ind))
         exp = exp[randomized_ind]
-        perm_min_pval = perform_regression(geno, exp, ind)["pval"].min(axis=1)
+        geno = geno[ind]
+        exp.columns, geno.columns = dummy_ind, dummy_ind # Need to match column names in order to call sm.OLS
+        perm_min_pval = perform_regression(geno, exp, dummy_ind)["pval"].min()
         return(perm_min_pval)
-
-    null_pval_dist = [permutation(geno, exp, ind) for _ in range(n)]
+    null_pval_dist = pd.Series([permutation(geno, exp, ind) for _ in range(n)])
     adj_pval = (null_pval_dist[null_pval_dist < min_pval].count().sum() + 1) / (n + 1)
     return(adj_pval)
 
 def correct_covariates(exp, ind, cov):
-    model = sm.OLS(exp.iloc[0], cov)
+    model = sm.OLS(exp.iloc[0], cov.T)
     results = model.fit()
-    resid_exp = results.resid
+    resid_exp = pd.DataFrame(results.resid).T
     return(resid_exp)
+
+def remove_nan(geno, exp):
+    all_ind = exp.columns
+    exp_dropna = exp.dropna(axis=1)
+    ind_dropna = exp_dropna.columns
+    dropped_ind = all_ind.difference(ind_dropna)
+    geno_dropna = geno.drop(dropped_ind, axis=1)
+    return(geno_dropna, exp_dropna, ind_dropna)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--genotypes', help='Genotypes of all individuals for SNPs within 100 kb of gene TSS.')
+    parser.add_argument('--phenotypes', help='Phenotypes of all individuals for the given gene.')
     parser.add_argument('--samples', help='File with sample IDs for individuals to use when calling eQTLs.')
-    parser.add_argument('--n_perm', help='Number of permutations to run.')
+    parser.add_argument('--n_perm', default=1000, help='Number of permutations to run.')
+    parser.add_argument('--type', help='est (estimation; no permutations) or asc (ascertainment; permutations performed)')
     parser.add_argument('--covariates', default=None, help='Covariates to include in regression.')
     parser.add_argument('--maf', default=0.05, help='MAF threshold for estimating variant effect size.')
     parser.add_argument('--ma_samples', default=5, help='Minor allele sample count threshold for estimating variant effect size.')
@@ -115,20 +127,49 @@ if __name__ == '__main__':
         n_perm = int(args.n_perm)
     except:
         print("Must specify integer number of permutations to run.")
-
-    ind_IDs = get_ind(args.samples)
-
-    expression = parse_expression(ind_IDs)
-    if args.covariates is not None:
-        covariates = parse_covariates(args.covariates, ind_IDs)
-        expression = correct_covariates(expression, ind_IDs, covariates)
-
-    genotypes = parse_genotypes(args.genotypes)
-    if genotypes is not None:
-        genotypes = maf_filter(genotypes, ind_IDs, args.maf, args.ma_samples)
-        results = perform_regression(genotypes, expression, ind_IDs)
-        candidate = results[results.pval == results.pval.min()]
     
-        cand_pval = candidate["pval"].iloc[0]
-        candidate["perm_pval"] = perm_adjust_pval(n_perm, cand_pval, genotypes, expression, ind_IDs)
-        candidate.to_csv(args.out, sep='\t')
+    fail_command = "touch {0}".format(args.out)
+    
+    print("Reading input files.")
+    ind_IDs = get_ind(args.samples)
+    expression = parse_expression(args.phenotypes, ind_IDs)
+    genotypes = parse_genotypes(args.genotypes, ind_IDs)
+    
+    print("Done reading input files.")
+    if genotypes is None: # Checks for the case in which there are no SNPs within 100 kb of the given gene's TSS
+        os.system(fail_command)
+    else: 
+        genotypes, expression, ind_IDs = remove_nan(genotypes, expression)
+        if args.covariates is not None:
+            covariates = parse_covariates(args.covariates, ind_IDs)
+            expression = correct_covariates(expression, ind_IDs, covariates)
+
+        genotypes = maf_filter(genotypes, ind_IDs, args.maf, args.ma_samples)
+        if genotypes.empty: # Checks for the case in which there are no SNPs in the desired MAF window
+            os.system(fail_command)
+        else:
+            results = perform_regression(genotypes, expression, ind_IDs)
+            results[geno_info_cols] = genotypes[geno_info_cols]
+            results.to_csv(args.out, sep='\t')
+
+#         print("Finished performing regression.")
+#         if args.type == 'ascertainment':
+#             print("Estimating eQTL for ascertainment dataset.")
+#             candidate = results[results.pval == results.pval.min()].sample(1) # Randomly choose SNP with min pval in case of tie
+#             cand_pval = candidate["pval"].iloc[0]
+#             if cand_pval < .10:
+#                 print("Adjusting p-value; performing 100 permutations.")
+#                 perm_pval = perm_adjust_pval(n_perm, cand_pval, genotypes, expression, ind_IDs)
+#             else:
+#                 print("No permutations performed; initial p-value > .10.")
+#                 candidate[["perm_pval", "n_perm"]] = [None, 0]
+#                 candidate.to_csv(args.out, sep='\t')
+#             # while perm_pval < .10 and n_perm <= 1000:
+#             #     n_perm = n_perm * 10
+#             #     print("Adjusting p-value; performing 1000 permutations.")
+#             #     perm_pval = perm_adjust_pval(n_perm, cand_pval, genotypes, expression, ind_IDs)
+#             candidate["perm_pval"] = perm_pval
+#             candidate["n_perm"] = n_perm
+#             candidate.to_csv(args.out, sep='\t')
+#         else:
+#             results.to_csv(args.out, sep='\t')
--- scripts/prep_covariates.py
+++ scripts/prep_covariates.py
@@ -1,19 +1,53 @@
 import pandas as pd
 import argparse
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from call_eqtls import parse_genotypes
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--samples", help="file mapping RNASeq samples to individual IDs")
 parser.add_argument("--indiv_metadata", help="file with individual metadata info")
 parser.add_argument("--sample_metadata", help="file with sample metadata info")
+parser.add_argument("--vcf", help="genotype VCF")
+parser.add_argument("--exp", help="normalized expression file")
 parser.add_argument("--out", help="output file")
 args = parser.parse_args()
 
+# Specify number of PCs
+num_exp_PC = 10
+num_geno_PC = 15
+
+def pca(n, X):
+    scaled = StandardScaler().fit_transform(X)
+    PCs = PCA(n_components=n).fit_transform(scaled)
+    PC_df = pd.DataFrame(PCs, columns=["PC" + str(i) for i in range(n)])
+    return(PC_df)
+
+# Prepare age, sex, and batch covariates
 samples = pd.read_csv(args.samples, delimiter='\t')
 indiv_metadata = pd.read_csv(args.indiv_metadata)
 sample_metadata = pd.read_csv(args.sample_metadata, delimiter='\t')
-
 samples = pd.merge(samples, sample_metadata[["TOR_ID", "BroadUWPlate", "Exam", "ageatExam"]])
 samples = pd.merge(samples, indiv_metadata[["TOR_ID_rnaseq", "gender1"]], left_on="TOR_ID", right_on="TOR_ID_rnaseq")
 samples = samples.drop("TOR_ID_rnaseq", axis=1).drop("TOR_ID", axis=1).drop_duplicates()
+
+# Generate expression PCs
+expression = pd.read_csv(args.exp, sep='\t')
+expression = expression.set_index("gene_id")
+expression = expression.drop(["#chr", "start", "end"], axis=1)
+expression = expression.dropna() # Remove genes where 1+ individuals have NaN values
+expression = expression.T # Convert to individual x gene matrix in order to generate PCs
+exp_PC = pca(num_exp_PC, expression) # Generate PCs on entire dataset
+exp_PC["NWDID"] = list(expression.index)
+
+# Generate genotype PCs
+genotypes = parse_genotypes(args.vcf)
+genotypes = genotypes.T # Convert to individual x genotype matrix in order to generate PCs
+geno_PC = pca(num_geno_PC, genotypes)
+geno_PC["NWDID"] = list(genotypes.index)
+
+# Merge all the covariates together
+PC_df = pd.merge(exp_PC, geno_PC, on="NWDID", suffixes=("_exp", "_geno"))
+samples = pd.merge(samples, PC_df, on="NWDID")
 
 samples.to_csv(args.out, sep='\t', index=False)
